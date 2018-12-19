c9466d8a6fd2f9bffa3d3efb586fb6e745f2d9a3
SharmaMohit <mohit.sharma@tatahealth.com>
2018-12-19 17:51:01 +0530

    Added Styling

--- src/client/containers/app/index.js
+++ src/client/containers/app/index.js
@@ -10,34 +10,36 @@ class App extends Component {
     return (
       <div className="App">
         <Display />
+        <div className="container">
         <table>
           <tbody>
             <tr>
               <th><button onClick={() => this.props.addInput("7")}>7</button></th>
               <th><button onClick={() => this.props.addInput("8")}>8</button></th>
               <th><button onClick={() => this.props.addInput("9")} >9</button></th>
-              <th><button onClick={() => this.props.addInput("/")} >/</button></th>
+              <th className="operator"><button onClick={() => this.props.addInput("/")} >/</button></th>
             </tr>
             <tr>
               <th><button onClick={() => this.props.addInput("4")} >4</button></th>
               <th><button onClick={() => this.props.addInput("5")} >5</button></th>
               <th><button onClick={() => this.props.addInput("6")} >6</button></th>
-              <th><button onClick={() => this.props.addInput("*")} >x</button></th>
+              <th className="operator"><button onClick={() => this.props.addInput("*")} >x</button></th>
             </tr>
             <tr>
               <th><button onClick={() => this.props.addInput("1")} >1</button></th>
               <th><button onClick={() => this.props.addInput("2")} >2</button></th>
               <th><button onClick={() => this.props.addInput("3")} >3</button></th>
-              <th><button onClick={() => this.props.addInput("-")} >-</button></th>
+              <th className="operator"><button onClick={() => this.props.addInput("-")} >-</button></th>
             </tr>
             <tr>
               <th><button onClick={() => this.props.addInput("0")} >0</button></th>
               <th><button onClick={() => this.props.addInput(".")} >.</button></th>
-              <th><button onClick={() => this.props.calculateResult()} >=</button></th>
-              <th><button onClick={() => this.props.addInput("+")} >+</button></th>
+              <th className="compute"><button onClick={() => this.props.calculateResult()} >=</button></th>
+              <th className="operator"><button onClick={() => this.props.addInput("+")} >+</button></th>
             </tr>
           </tbody>
         </table>
+        </div>
       </div>
     )
   }
